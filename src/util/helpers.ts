243305691068155b8bd32630f06c3d9e01ffe4c2
yinm <yinm.cminor9@gmail.com>
2018-08-21 22:22:24 +0900

    Add filterObjectOnKeys()

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -19,3 +19,15 @@ export function createSimpleFunctional (
   }
 }
 
+export function filterObjectOnKeys<T, K extends keyof T> (obj: T, keys: K[]): { [N in K]: T[N] } {
+  const filtered = {} as { [N in K]: T[N] }
+
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i]
+    if (typeof obj[key] !== 'undefined') {
+      filtered[key] = obj[key]
+    }
+  }
+
+  return filtered
+}
